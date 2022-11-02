1e72d6638ea4e160c71e7c0d83f79d874ca66cdb
Oleksandr.Zoria <oleksandr.zoria@p2h.com>
2022-11-02 21:26:24 +0200

    validate required field

--- src/js/register.js
+++ src/js/register.js
@@ -6,37 +6,72 @@ import register from '../styles/pages/register.scss'
     constructor(formSelector, opts) {
       this.form = formSelector;
       this.opts = opts;
-      form.addEventListener('submit', function (e) {
-        e.preventDefault();
-      });
     }
 
     get options() {
       return Object.entries(this.opts);
     }
 
+    validateRequired(selector) {
+      if (!selector.value.length) {
+        selector.classList.add('is-danger');
+        const wrapper = selector.parentNode;
+        if (wrapper.nextElementSibling) {
+          return false;
+        }
+        const html = "<p class=\"help is-danger\">This field is required</p>";
+        wrapper.insertAdjacentHTML("afterend", html);
+        return false;
+      } else if (selector.classList.contains('is-danger')) {
+        selector.classList.remove('is-danger');
+        const wrapper = selector.parentNode;
+        const node = wrapper.nextElementSibling;
+        node.remove();
+        return true;
+      } else {
+        return true;
+      }
+    }
+
+    validateMinLength(selector, opts) {
+      return true;
+    }
+
+    validateMaxLength(selector, opts) {
+      return true;
+    }
+
     validateInput(selector, opts) {
-      console.log('selector', selector);
-      console.log('opts', opts);
+      let isValid = false;
+      for (const option in opts) {
+        switch(option){
+          case 'required':
+            isValid = this.validateRequired(selector, opts);
+            break;
+          case 'minLength':
+            isValid = this.validateMinLength(selector, opts);
+            break;
+          case 'maxLength':
+            isValid = this.validateMaxLength(selector, opts);
+            break;
+        }
+      }
+      return isValid;
     }
 
     validate() {
+      let isValid = false;
       this.options.forEach((opt) => {
         const itemName = opt[0];
         const validationOptions = opt[1];
         const inputItem = this.form.querySelector(`[data-name=${itemName}]`);
-        this.validateInput(inputItem, validationOptions);
+        isValid = this.validateInput(inputItem, validationOptions);
       });
-      return true;
+      return isValid;
     }
   }
 
   const form = document.querySelector('#form');
-  const email = form.querySelector('[data-name="email"]');
-  const password = form.querySelector('[data-name="password"]');
-  console.log('email', email);
-  console.log('password', password);
-  console.log(form);
 
   const formValidator = new FormValidator(form, {
     'email': {
@@ -48,4 +83,9 @@ import register from '../styles/pages/register.scss'
       required: true
     }
   })
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(formValidator.validate());
+  });
 })()
