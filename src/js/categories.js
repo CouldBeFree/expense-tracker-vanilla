8efdc391f4853ae11daedb49dcbdc4f389a99f67
CouldBeFree <zoriaalexandr@gmail.com>
2022-11-16 16:16:33 +0200

    created remove and edit modal

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,5 +1,46 @@
 import categories from '../styles/pages/categories.scss';
 
 (function () {
+  class CategoriesHandler {
+    constructor() {
+      this.removeButtons = document.querySelectorAll('.remove');
+      this.editButtons = document.querySelectorAll('.edit');
+      this.removePopup = document.querySelector('#remove-modal');
+      this.closeRemoveModal = document.querySelectorAll('.delete');
+      this.modalBackground = document.querySelectorAll('.modal-background');
+      this.editModal = document.querySelector('#edit-modal');
+    }
 
+    addHandlerForRemove() {
+      this.removeButtons.forEach(el => {
+        el.addEventListener('click', () => {
+          this.removePopup.classList.add('is-active');
+        })
+      });
+      this.editButtons.forEach(el => {
+        el.addEventListener('click', () => {
+          this.editModal.classList.add('is-active');
+        })
+      });
+      this.closeRemoveModal.forEach(el => {
+        el.addEventListener('click', () => {
+          this.removePopup.classList.remove('is-active');
+          this.editModal.classList.remove('is-active');
+        });
+      });
+      this.modalBackground.forEach(el => {
+        el.addEventListener('click', () => {
+          this.removePopup.classList.remove('is-active');
+          this.editModal.classList.remove('is-active');
+        });
+      });
+    }
+
+    init() {
+      this.addHandlerForRemove();
+    }
+  }
+
+  const handler = new CategoriesHandler();
+  handler.init();
 })();
